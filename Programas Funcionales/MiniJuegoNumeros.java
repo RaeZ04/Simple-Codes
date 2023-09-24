6fc4e200971a093cc2ab9ff225bdc7dd158652fb
Sergio <sergiolopezmu2004@gmail.com>
2023-09-24 05:09:33 +0200

    Última versión

---
+++ Programas Funcionales/MiniJuegoNumeros.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class practicaBucles2 {
+
+    public static void main (String [] args ){
+
+        int random = (int) (Math.random()*100);
+
+        Scanner entrada = new Scanner(System.in);
+
+        int num = 0;
+
+        int intentos = 0;
+
+        while(num!=random){
+
+            intentos++;
+
+            System.out.println("Introduce un número");
+
+            num = entrada.nextInt();
+
+            if (random<num){
+
+                System.out.println("\nMás bajo \n");
+
+            }
+
+            else if (random>num){
+
+                System.out.println("\nMás alto \n");
+
+            }
+
+        }
+
+        System.out.println("Correcto!, has acertado en " + intentos + " intentos!");
+
+    }
+    
+}
