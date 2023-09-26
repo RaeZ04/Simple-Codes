4f1d10309785ab55338089e11fa62bf7b3028bca
Sergio <sergiolopezmu2004@gmail.com>
2023-09-26 21:55:48 +0200

    Ultima Version

--- Programas Funcionales/miniJuegoCalculos.java
+++ Programas Funcionales/miniJuegoCalculos.java
@@ -1,84 +1,82 @@
 import javax.swing.JOptionPane;
 
 public class miniJuegoCalculos {
-    
-    public static void main (String [] args){
-        
+
+    public static void main (String[] args) {
+
         int aciertosRestantes = 10;
 
         int vida = 100;
 
         String SiNo = JOptionPane.showInputDialog("¿Quieres empezar a jugar? (Elije entre S/N)");
 
-        if (SiNo.equalsIgnoreCase("S")){
+        if (SiNo.equalsIgnoreCase("S")) {
 
-                    while (aciertosRestantes>0){
+            System.out.println("Empiezas con 100 de vida, buena suerte.");
 
-            int random1 = (int) (Math.random()*100);
-            int random2 = (int) (Math.random()*100);
-            int random3 = (int) ((Math.random()*20)+1);
-            
-            
+            while (aciertosRestantes > 0) {
 
-            int resultado = random1 + random2;
-            
+                int random1 = (int) (Math.random() * 100);
+                int random2 = (int) (Math.random() * 100);
+                int random3 = (int) ((Math.random() * 20) + 1);
 
-            int pregunta = Integer.parseInt(JOptionPane.showInputDialog("Cuánto es " + random1 + "+" + random2));
+                int resultado = random1 + random2;
 
-            if (pregunta==resultado) {
+                int pregunta = Integer.parseInt(JOptionPane.showInputDialog("Cuánto es " + random1 + "+" + random2));
 
-                aciertosRestantes--;
+                if (pregunta == resultado) {
 
-                if(aciertosRestantes!=0){
+                    aciertosRestantes--;
 
-                    System.out.println("Has acertado, te quedan " + aciertosRestantes + " intentos.");
+                    if (aciertosRestantes != 0) {
+
+                        System.out.println("Has acertado, te quedan " + aciertosRestantes + " intentos.");
+
+                    }
+
+                    else if (aciertosRestantes == 0) {
+
+                        System.out.println("¡ENHORABUENA, has ganado!!!");
+
+                    }
+
+                }
+
+                else {
+
+                    vida = vida - random3;
+
+                    if (vida > 0) {
+
+                        System.out.println("Has fallado, te queda " + vida + " de vida.");
+
+                    }
 
                 }
 
-                else if (aciertosRestantes==0){
+                if (vida <= 0) {
 
-                    System.out.println("¡ENHORABUENA, has ganado!!!");
-
-                }
-                
-
-            }
-
-            else{
-
-                vida = vida-random3;
-
-                if (vida>0){
-
-                    System.out.println("Has fallado, te queda " + vida + " de vida.");
+                    System.out.println("Tu vida ha llegado a 0, has perdido.");
+                    break;
 
                 }
-               
-
-            }
-
-            if (vida<=0){
-
-                System.out.println("Tu vida ha llegado a 0, has perdido.");
-                break;
 
             }
 
         }
 
-        }
-
-        else{
+        else if (SiNo.equalsIgnoreCase("n")) {
 
             System.out.println("Has decidio no jugar :(");
 
         }
 
+        else {
+
+            System.out.println("La opción que has elegido no es valida, tienes que ingresar una 'S' o una 'N'.");
+
+        }
 
     }
-   
-        
+
 }
-
-
-
