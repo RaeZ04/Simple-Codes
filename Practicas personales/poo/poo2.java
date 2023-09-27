2b4dad320fb8db39657e48a70b911b3a7e85dce7
Sergio <sergiolopezmu2004@gmail.com>
2023-09-27 14:16:08 +0200

    Ultima Version

---
+++ Practicas personales/poo/poo2.java
@@ -0,0 +1,16 @@
+package poo;
+
+public class poo2 {
+
+    public static void main (String [] args){
+
+    poo1 Coche = new poo1();
+
+    System.out.println("Este coche tiene " + Coche.ruedas + " ruedas.");
+
+    }
+
+
+
+    
+}
