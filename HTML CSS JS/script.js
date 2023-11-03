d1383b16e7f101b85554beadacf7ae1fcb4987f3
Sergio <sergiolopezmu2004@gmail.com>
2023-11-03 03:53:40 +0100

    Ultima Version

---
+++ HTML CSS JS/script.js
@@ -0,0 +1,19 @@
+function boton1() {
+    
+    alert ('Inicio sesion')
+
+}
+
+function mueveReloj(){
+    momentoActual = new Date()
+    hora = momentoActual.getHours()
+    minuto = momentoActual.getMinutes()
+    segundo = momentoActual.getSeconds()
+
+    horaImprimible = hora + " : " + minuto + " : " + segundo
+
+    document.form_reloj.reloj.value = horaImprimible
+
+    setTimeout("mueveReloj()",1000)
+
+}
